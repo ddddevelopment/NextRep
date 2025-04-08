72a6148768a4621c7a3449a9ae1c7598ac6753ef
Dmitrii <dgorbacev742@gmail.com>
2025-04-08 19:52:43 +0300

    add validation in UserEntity

--- src/Services/Users/Users.DAL.EF/Entities/UserEntity.cs
+++ src/Services/Users/Users.DAL.EF/Entities/UserEntity.cs
@@ -4,8 +4,17 @@ namespace Users.DAL.Entities {
     public class UserEntity {
         [Key]
         public Guid id { get; set; }
+
+        [Required(ErrorMessage = "name is required")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "name length must be greater than 2 and less than 50")]
         public string name { get; set; }
+
+        [Required(ErrorMessage = "email is required")]
+        [EmailAddress(ErrorMessage = "email must be a valid email address")]
         public string email { get; set; }
+
+        [Required(ErrorMessage = "telephone is required")]
+        [RegularExpression(@"^\+?[1-9]\d{1,14}$", ErrorMessage = "telephone must be a valid phone number")]
         public string telephone { get; set; }
     }
 }
