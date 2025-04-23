8037159fdd756bb32b47cf9d2b41cee6b85570ff
Dmitrii <dgorbacev742@gmail.com>
2025-04-23 16:53:15 +0300

    add Guid generation

--- src/Services/Users/Users.Domain/Models/User.cs
+++ src/Services/Users/Users.Domain/Models/User.cs
@@ -2,12 +2,12 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Users.Domain.Models {
     public class User {
-        public Guid Id { get; set; }
+        public Guid Id { get; set; } = Guid.NewGuid();
         public string Name { get; set; }
         public string Email { get; set; }
         public string Telephone { get; set; }
         public string PasswordHash { get; set; }
         public List<Role> Roles { get; set; } = new List<Role>();
-        public bool IsActive { get; set; }
+        public bool IsActive { get; set; } = true;
     }
 }
