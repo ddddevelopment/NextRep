30e3b7486178b74b73944b0deb2c5408fa31adb6
Dmitrii <dgorbacev742@gmail.com>
2025-05-13 23:11:10 +0300

    assembly

--- src/Services/Auth/Auth.Api/obj/Debug/net9.0/Auth.Api.AssemblyInfo.cs
+++ src/Services/Auth/Auth.Api/obj/Debug/net9.0/Auth.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Auth.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68bbbb2404313826fe99004707ad7526b9cd4e85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5cd4d43f3adb6f9a894946f85679ee5fdd4a8ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Auth.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Auth.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
